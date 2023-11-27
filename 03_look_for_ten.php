eb778d45529c233399778f0a18282ff812002a07
Rajesh Mondal <rajesh01749@gmail.com>
2023-11-27 17:27:03 +0600

    look for ten (problem-3)

---
+++ 03_look_for_ten.php
@@ -0,0 +1,10 @@
+<?php
+    # Write your PHP code from here
+    fscanf( STDIN, "%d %d", $a, $b );
+
+    if ( $a == 10 || $b == 10 || ( $a + $b ) == 10 ) {
+        print "True";
+    } else {
+        print "False";
+    }
+?>
